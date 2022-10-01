85b68bb01e8b862bf1f9b1cf37cbfdd0aee26f89
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-10-01 17:13:07 -0300

    start project

---
+++ src/main/java/io/helioanacronista/CrudCliente/CrudClienteApplication.java
@@ -0,0 +1,13 @@
+package io.helioanacronista.CrudCliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudClienteApplication.class, args);
+	}
+
+}
