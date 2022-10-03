46c59d1ca52857a6c682c82e8fc530a06b76bf12
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-10-03 14:43:20 -0300

    crud - findAll, insert, update , dalete

--- src/main/java/io/helioanacronista/CrudCliente/controllers/ClientControllers.java
+++ src/main/java/io/helioanacronista/CrudCliente/controllers/ClientControllers.java
@@ -3,20 +3,40 @@ package io.helioanacronista.CrudCliente.controllers;
 import io.helioanacronista.CrudCliente.dto.ClientDTO;
 import io.helioanacronista.CrudCliente.services.ClientService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/clients")
 public class ClientControllers {
 
-   @Autowired
-   private ClientService service;
+    @Autowired
+    private ClientService service;
 
-   @GetMapping(value = "/{id}")
-    public ClientDTO findById (@PathVariable Long id) {
-       return service.findById(id);
-   }
+    @GetMapping(value = "/{id}")
+    public ClientDTO findById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
+    @GetMapping
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        return service.findAll(pageable);
+    }
+
+    @PostMapping
+    public ClientDTO insert(@RequestBody ClientDTO dto) {
+        return service.insert(dto);
+    }
+
+    @PutMapping(value = "/{id}")
+    public ClientDTO update(@PathVariable Long id, @RequestBody ClientDTO dto) {
+        dto = service.update(id, dto);
+        return dto;
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public void delete (@PathVariable Long id) {
+        service.delete(id);
+    }
 }
--- src/main/java/io/helioanacronista/CrudCliente/services/ClientService.java
+++ src/main/java/io/helioanacronista/CrudCliente/services/ClientService.java
@@ -4,6 +4,8 @@ import io.helioanacronista.CrudCliente.dto.ClientDTO;
 import io.helioanacronista.CrudCliente.entities.Client;
 import io.helioanacronista.CrudCliente.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -13,9 +15,46 @@ public class ClientService {
     @Autowired
     private ClientRepository repository;
 
-    @Transactional()
+    @Transactional(readOnly = true)
     public ClientDTO findById(Long id) {
         Client client = repository.findById(id).get();
         return new ClientDTO(client);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<ClientDTO> findAll(Pageable pageable) {
+        Page<Client> result = repository.findAll(pageable);
+        return result.map(x -> new ClientDTO(x));
+    }
+
+    @Transactional
+    public ClientDTO insert(ClientDTO dto) {
+        Client entity = new Client();
+
+        copyToEntityDTO(dto, entity);
+
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public ClientDTO update (Long id, ClientDTO dto) {
+        Client entity = repository.getReferenceById(id);
+        copyToEntityDTO(dto, entity);
+        entity = repository.save(entity);
+        return new ClientDTO(entity);
+    }
+
+    @Transactional
+    public void delete (Long id) {
+        repository.deleteById(id);
+    }
+
+    private void copyToEntityDTO(ClientDTO dto, Client entity) {
+        entity.setName(dto.getName());
+        entity.setCpf(dto.getCpf());
+        entity.setIncome(dto.getIncome());
+        entity.setBirthDate(dto.getBirthDate());
+        entity.setChildren(dto.getChildren());
     }
 }
